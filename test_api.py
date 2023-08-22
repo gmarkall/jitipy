b10c799ff1c18ef5ed244d3ce87bc252c2851dcc
Graham Markall <gmarkall@nvidia.com>
2023-08-22 17:25:33 +0100

    Delete the cache to avoid the leak

--- test_api.py
+++ test_api.py
@@ -5,3 +5,4 @@ code = "my_program\n__global__ void my_kernel(int *data) { *data = 1; }"
 cache = jitipy.create_jit_cache()
 program = jitipy.jit_cache_program(cache, code)
 jitipy.delete_program(program)
+jitipy.delete_jit_cache(cache)
