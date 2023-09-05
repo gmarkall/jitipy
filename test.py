4248581217d93098c0138facf29301699ab55845
Graham Markall <gmarkall@nvidia.com>
2023-09-05 14:29:05 +0100

    Add print to test file

--- test.py
+++ test.py
@@ -6,6 +6,10 @@ interpreter = jitipy.create_interpreter()
 print("Include iostream")
 jitipy.parse_and_execute(interpreter, "#include <iostream>")
 
+print("Printing")
+jitipy.parse_and_execute(interpreter,
+                         'std::cout << "Hello, world!" << std::endl;')
+
 print("Delete interpreter")
 jitipy.delete_interpreter(interpreter)
 
