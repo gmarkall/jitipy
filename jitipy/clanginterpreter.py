c31d0b586938abe59768710502db23efc8dc6e6f
Graham Markall <gmarkall@nvidia.com>
2023-09-06 16:43:38 +0100

    Add support for bringing values back to Python

--- jitipy/clanginterpreter.py
+++ jitipy/clanginterpreter.py
@@ -3,6 +3,36 @@
 import ctypes
 import pathlib
 
+
+class Storage(ctypes.Union):
+    _fields_ = [("m_Bool", ctypes.c_bool),
+                ("m_Char_S", ctypes.c_char),
+                ("m_SChar", ctypes.c_char),
+                ("m_UChar", ctypes.c_uint8),
+                ("m_Short", ctypes.c_short),
+                ("m_UShort", ctypes.c_ushort),
+                ("m_Int", ctypes.c_int),
+                ("m_UInt", ctypes.c_uint),
+                ("m_Long", ctypes.c_long),
+                ("m_ULong", ctypes.c_ulong),
+                ("m_LongLong", ctypes.c_longlong),
+                ("m_ULongLong", ctypes.c_ulonglong),
+                ("m_Float", ctypes.c_float),
+                ("m_Double", ctypes.c_double),
+                ("m_LongDouble", ctypes.c_longdouble),
+                ("m_Ptr", ctypes.c_void_p)]
+
+
+class Value(ctypes.Structure):
+    _fields_ = [("Interp", ctypes.c_void_p),
+                ("OpaqueType", ctypes.c_void_p),
+                ("Data", Storage),
+                ("ValueKind", ctypes.c_int),
+                ("IsManuallyAlloc", ctypes.c_bool)]
+
+
+ValuePointer = ctypes.POINTER(Value)
+
 DEBUG_EXECUTION = False
 
 sopath = pathlib.Path(__file__).absolute().parent / 'lib_clanginterpreter.so'
@@ -33,7 +63,7 @@ def llvm_shutdown():
 
 
 def parse_and_execute(interpreter, code):
-    result = ctypes.c_int(0)
+    result = ctypes.c_uint64(0)
 
     if isinstance(code, str):
         code = (code,)
@@ -56,7 +86,7 @@ def parse_and_execute(interpreter, code):
 
         continuation = ''
 
-    return result.value
+    return ctypes.cast(result.value, ValuePointer)
 
 
 def load_dynamic_library(interpreter, name):
--- test_jitify_result.py
+++ test_jitify_result.py
@@ -2,5 +2,5 @@ import jitipy
 
 interpreter = jitipy.create_interpreter()
 
-result = jitipy.parse_and_execute(interpreter, '1')
-print(result)
+result = jitipy.parse_and_execute(interpreter, '255')
+print(result[0].Data.m_Int)
