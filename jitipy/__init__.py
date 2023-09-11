d88eddec7e85ebb9ba0adfbe7997cc024fc74216
Graham Markall <gmarkall@nvidia.com>
2023-09-11 15:37:00 +0100

    Tidy up some debug prints

--- jitipy/__init__.py
+++ jitipy/__init__.py
@@ -63,7 +63,6 @@ class Interpreter:
 
 def construct_cpp_variable(ty, ptr):
     variable = f"(*(reinterpret_cast<{ty}*>(0x{ptr:x}ull)))"
-    print(f"Variable: {variable}")
     return variable
 
 
@@ -80,10 +79,6 @@ auto {var} = {jitify_object}->configure({grid_dim}, {block_dim}); {var}"""
 
 launch_code = """\
 {jitify_object}->launch({args});"""
-
-test_kernel_code = """\
-float *data = reinterpret_cast<float*>(0x{data:x}ull);
-{program}->preprocess()->get_kernel("my_kernel")->configure(1, 1)->launch(data);"""
 
 
 class JitifyObject:
@@ -103,21 +98,14 @@ class JitifyObject:
 class Program(JitifyObject):
     def __init__(self, name, source):
         code = program_code.format(name=name, source=source)
-        print(f"Program code:\n\n{code}")
         self._value = get_interpreter().parse_and_execute(code)
         self._ty = "jitify2::Program"
 
     def preprocess(self):
         code = preprocess_program_code.format(jitify_object=self.variable,
                                               var=_new_variable())
-        print(f"Preprocess program code:\n\n{code}")
         value = get_interpreter().parse_and_execute(code)
         return PreprocessedProgram(value)
-
-    def test_kernel(self, data):
-        code = test_kernel_code.format(program=self.variable, data=data)
-        print(f"Preprocess program code:\n\n{code}")
-        get_interpreter().parse_and_execute(code)
 
 
 class PreprocessedProgram(JitifyObject):
@@ -128,7 +116,6 @@ class PreprocessedProgram(JitifyObject):
     def get_kernel(self, name):
         code = get_kernel_code.format(jitify_object=self.variable, name=name,
                                       var=_new_variable())
-        print(f"Get kernel code:\n\n{code}")
         value = get_interpreter().parse_and_execute(code)
         return Kernel(value)
 
@@ -143,7 +130,6 @@ class Kernel(JitifyObject):
                                      grid_dim=grid_dim,
                                      block_dim=block_dim,
                                      var=_new_variable())
-        print(f"Configure code:\n\n{code}")
         value = get_interpreter().parse_and_execute(code)
         return ConfiguredKernel(value)
 
@@ -158,14 +144,4 @@ class ConfiguredKernel(JitifyObject):
         args = ", ".join(str(arg) for arg in args)
         code = launch_code.format(jitify_object=self.variable,
                                   args=args)
-        print(f"Launch code:\n\n{code}")
         get_interpreter().parse_and_execute(code)
-
-
-__all__ = (
-    'create_interpreter',
-    'delete_interpreter',
-    'llvm_shutdown',
-    'load_dynamic_library'
-    'parse_and_execute',
-)
--- jitipy/tests/conftest.py
+++ jitipy/tests/conftest.py
@@ -8,5 +8,4 @@ from jitipy import clanginterpreter
 @pytest.fixture(scope='session')
 def llvm_shutdown_at_end():
     yield
-    print("LLVM shutdown")
     clanginterpreter.llvm_shutdown()
