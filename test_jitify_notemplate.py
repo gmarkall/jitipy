b80f7e6c87c3fde7ba5ef1d0a436c8442a9da404
Graham Markall <gmarkall@nvidia.com>
2023-09-11 10:37:16 +0100

    Add version of program test without template

---
+++ test_jitify_notemplate.py
@@ -0,0 +1,23 @@
+import jitipy
+import sys
+
+source = """
+template<int N, typename T>
+__global__
+void my_kernel(T* data) {
+    T data0 = data[0];
+    for( int i=0; i<N-1; ++i ) {
+        data[0] *= data0;
+    }
+}
+"""
+
+
+program = jitipy.Program("my_program", source)
+print(f'0x{program.ptr:x}')
+#input()
+
+#sys.exit(0)
+
+preprocessed_program = program.preprocess()
+print(f'0x{preprocessed_program.ptr:x}')
