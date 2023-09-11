6d91dfd17fea9b2a5bc597dc9c9df59b2c4293d1
Graham Markall <gmarkall@nvidia.com>
2023-09-11 15:01:49 +0100

    Move test_jitify_program tests into pytest

--- jitipy/tests/test_interpreter.py
+++ jitipy/tests/test_interpreter.py
@@ -4,12 +4,6 @@ import pytest
 from jitipy import clanginterpreter
 
 
-@pytest.fixture(scope='session')
-def llvm_shutdown_at_end():
-    yield
-    clanginterpreter.llvm_shutdown()
-
-
 @pytest.fixture
 def interpreter(llvm_shutdown_at_end):
     interpreter = clanginterpreter.create_interpreter()
