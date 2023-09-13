0c71fd964a6ff0f8d0e2f56592724b46c507a5cf
Graham Markall <gmarkall@nvidia.com>
2023-09-13 12:42:39 +0100

    Add some more detail to example

--- example.py
+++ example.py
@@ -1,8 +1,17 @@
+# Copyright (c) 2023, NVIDIA CORPORATION. All rights reserved.
+
+# This file demonstrates the use of Jitipy, a Python API to Jitify backed by
+# the Clang interpreter, to instantiate a simple kernel and launch it.
+
+
+# Main library
 import jitipy
+
+# Use to create and manage test data
 import numpy as np
 from numba import cuda
 
-
+# A simple templated kernel
 source = """
 #include <stdint.h>
 
@@ -16,10 +25,23 @@ void my_kernel(T* data) {
 }
 """
 
+# Program creation
 program = jitipy.Program("my_program", source)
+
+
+# Instantiation for float32 data
 data = cuda.to_device(np.asarray([5], dtype=np.float32))
 data_ptr = data.__cuda_array_interface__['data'][0]
 kernel = program.preprocess().get_kernel("my_kernel<3, float>")
 kernel.configure(1, 1).launch(data_ptr)
 
-print(data[0])
+print(data[0])  # prints 125.0
+
+
+# Instantiation for int64 data
+data = cuda.to_device(np.asarray([5], dtype=np.int64))
+data_ptr = data.__cuda_array_interface__['data'][0]
+kernel = program.preprocess().get_kernel("my_kernel<4, int64_t>")
+kernel.configure(1, 1).launch(data_ptr)
+
+print(data[0])  # prints 625
